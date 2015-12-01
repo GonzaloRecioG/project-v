580f3820c307bfd78324b020f033aaa693f6907f
Gus De Vita <gdevita@gmail.com>
2015-12-01 14:55:10 +0100

    Routes, index, DB

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  get 'experiences/index'
-
-  get 'experiences/new'
-
-  get 'experiences/create'
-
-  get 'experiences/show'
-
-  get 'experiences/destroy'
+  resources :experiences, only: [:index, :new, :show, :create, :destroy]
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -63,4 +56,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
