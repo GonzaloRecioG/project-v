632ecc4bb42a5a80dbecfd0f2abb2469c9152bfa
Gonzalo Recio <gon@reciogarcia.com>
2015-12-02 16:22:57 +0100

    video background

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -2,6 +2,7 @@ class ExperiencesController < ApplicationController
   before_action :authenticate_user!
   skip_before_action :authenticate_user!, only: :index
 
+
   def index
     @experiences = Experience.where.not(latitude: nil) #.all
 
