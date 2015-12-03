249e242567c793a716eea6e662fd5a0010aa4eb8
Amiran Yaghout <amiranyaghout@gmail.com>
2015-12-03 14:43:10 +0100

    userrating added

---
+++ app/models/userrating.rb
@@ -0,0 +1,10 @@
+class Userrating < ActiveRecord::Base
+  belongs_to :user
+
+   def set_user!(user)
+    self.user_id = user.id
+
+    self.save!
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202182115) do
+ActiveRecord::Schema.define(version: 20151203112442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,15 @@ ActiveRecord::Schema.define(version: 20151202182115) do
 
   add_index "reviews", ["experience_id"], name: "index_reviews_on_experience_id", using: :btree
 
+  create_table "userratings", force: :cascade do |t|
+    t.string   "comment"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "userratings", ["user_id"], name: "index_userratings_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -86,4 +95,5 @@ ActiveRecord::Schema.define(version: 20151202182115) do
   add_foreign_key "bookings", "users"
   add_foreign_key "experiences", "users"
   add_foreign_key "reviews", "experiences"
+  add_foreign_key "userratings", "users"
 end
